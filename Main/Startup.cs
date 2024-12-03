94f6bf957f592a5f189a53eca89c7f1df2acdca2
NaoriYchiha <lolollb770@gmail.com>
2024-12-03 19:25:34 +0200

    clean project

---
+++ Main/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Main.Startup))]
+namespace Main
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
